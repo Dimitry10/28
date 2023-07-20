3baa6b14833d5acd6796c2b68cd67e16fabe81d7
Dimitry10 <136828074+Dimitry10@users.noreply.github.com>
2023-07-20 14:55:34 +0300

    main.py
    Строка итерируемый объект,
    следовательно можно сделать reverse таким образом,
    то есть сделать проверку на полиндром строки

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-#def strcount(s):
-#    for sym in set(s):
- #       counter = 0
- #       for sub_sym in s:
-  #          if sym == sub_sym:
- #               counter+=1
- #       print(sym,counter)
+word = input("Напишите слово: ")
 
-
-
-
-def strcount(s):
-    dct={}
-    for sym in s:
-        dct[sym]=dct.get(sym,0)+1
-    for key,value in dct.items():
-        print(key,value)
-strcount('abac')
+if word.lower() == word.lower()[::-1]:
+    print(True)
+else:
+    print(False)
